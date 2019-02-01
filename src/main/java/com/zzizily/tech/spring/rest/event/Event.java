851ea61635e31d26e86d8af0c9b060c9e4c897f4
SeokYoung.Kim <deuxksy@gmail.com>
2019-02-01 15:30:22 +0900

    swagger 기능 추가

--- src/main/java/com/zzizily/tech/spring/rest/event/Event.java
+++ src/main/java/com/zzizily/tech/spring/rest/event/Event.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
 @Entity
-public class Event {
+public class Event{
   @Id
   @GeneratedValue
   private Integer id;
--- src/main/java/com/zzizily/tech/spring/rest/event/EventDTO.java
+++ src/main/java/com/zzizily/tech/spring/rest/event/EventDTO.java
@@ -1,16 +1,25 @@
 package com.zzizily.tech.spring.rest.event;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "Class representing a person tracked by the application.")
 public class EventDTO {
+  @NotBlank
+  @Size(min=3, max=20)
+  @ApiModelProperty(notes = "First name of the person.", example = "John", required = true, position = 1)
+  private String firstName;
   private String name;
   private String description;
   private LocalDateTime beginEnrollmentDateTime;
