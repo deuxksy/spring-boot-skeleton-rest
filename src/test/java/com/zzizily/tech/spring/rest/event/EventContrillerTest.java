b112e08ae203c952de6b1410516c6c38e9e758a4
SeokYoung.Kim <deuxksy@gmail.com>
2019-02-01 15:33:09 +0900

    BadRequest 발생 시켜 보기

--- src/test/java/com/zzizily/tech/spring/rest/event/EventContrillerTest.java
+++ src/test/java/com/zzizily/tech/spring/rest/event/EventContrillerTest.java
@@ -1,15 +1,11 @@
 package com.zzizily.tech.spring.rest.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.tomcat.jni.Local;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -35,26 +31,45 @@ public class EventContrillerTest {
 
   @Test
   public void createEvnet() throws Exception {
-    Event event = Event.builder()
-      .id(100)
-      .name("")
-      .description("")
-      .beginEnrollmentDateTime(LocalDateTime.of(2019, 1, 27, 11, 34))
-      .closeEnrollmentDateTime(LocalDateTime.of(2019, 3, 1, 1, 1))
-      .build();
+    EventDTO eventDTO = EventDTO.builder()
+            .name("설 이벤트")
+            .description("좀 사주세요 매출 채워야 해요")
+            .beginEnrollmentDateTime(LocalDateTime.of(2019, 1, 27, 11, 34))
+            .closeEnrollmentDateTime(LocalDateTime.of(2019, 3, 1, 1, 1))
+            .build();
 
     mockMvc.perform(
-      post("/api/events")
-        .contentType(MediaType.APPLICATION_JSON_UTF8)
-        .accept(MediaTypes.HAL_JSON)
-        .content(objectMapper.writeValueAsString(event)
-      )
+            post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(eventDTO)
+                    )
     )
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("id").exists())
+            .andExpect(header().exists(HttpHeaders.LOCATION))
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+    ;
+  }
+
+  @Test
+  public void createEvnetBadRequest() throws Exception {
+    Event eventDTO = Event.builder()
+    .id(10)
+    .name("설 이벤트")
+    .description("좀 사주세요 매출 채워야 해요")
+    .beginEnrollmentDateTime(LocalDateTime.of(2019, 1, 27, 11, 34))
+    .closeEnrollmentDateTime(LocalDateTime.of(2019, 3, 1, 1, 1))
+    .build();
+
+    mockMvc.perform(post("/api/events")
+      .contentType(MediaType.APPLICATION_JSON_UTF8)
+      .accept(MediaTypes.HAL_JSON)
+      .content(objectMapper.writeValueAsString(eventDTO)
+    ))
     .andDo(print())
-    .andExpect(status().isCreated())
-    .andExpect(jsonPath("id").exists())
-    .andExpect(header().exists(HttpHeaders.LOCATION))
-    .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+    .andExpect(status().isBadRequest())
     ;
   }
 }
